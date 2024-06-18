cd5f63b31a8ddeb9c4e8dcbaf3467097d5008a23
Roshan Jose <roshanstark007@gmail.com>
2024-06-18 19:34:26 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route('/.well-known/assetlinks.json')
-def get_assetlinks():
+def get_asset_links():
     return send_from_directory('.well-known', 'assetlinks.json')
-
-
